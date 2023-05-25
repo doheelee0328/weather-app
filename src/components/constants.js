df412ff6a0ad313bb71bf26894d3d0d425ba9d39
Dohee Lee <dojuhee2015@gmail.com>
2023-05-25 02:08:53 +0100

    remove the key

--- src/components/constants.js
+++ src/components/constants.js
@@ -1,5 +1,5 @@
 const WEATHER_API_URL = 'http://api.weatherapi.com/v1'
 const API_KEY = process.env.REACT_APP_WEATHER_API_KEY
 
-export const WEATHER_FETCH_ENDPOINT = `${WEATHER_API_URL}/current.json?key=673230918b084e85977163833231802&q= `
+export const WEATHER_FETCH_ENDPOINT = `${WEATHER_API_URL}/current.json?key=${API_KEY}&q= `
 export const FORECAST_FETCH_ENDPOINT = `${WEATHER_API_URL}/forecast.json?key=${API_KEY}&q=`
